2fc9968fc0f9d2f20e2e07eb4c30e1793ba9342d
Jim Marshall <jim@serendipityai.co.uk>
2025-06-05 15:34:44 +0100

    Add alby example

---
+++ examples/alby_payment.py
@@ -0,0 +1,19 @@
+from markdown2pdf import MarkdownPDF
+from dotenv import load_dotenv
+from pyalby import Account, Invoice, Payment
+
+load_dotenv()
+# In your .env file, ensure you have set the following:
+# BASE_URL = https://api.getalby.com
+# ALBY_ACCESS_TOKEN = <your_alby_access_token>
+# LOG_LEVEL = INFO
+
+def pay(offer):
+    print("Paying using Alby:")
+    payment = Payment()
+    pay = payment.bolt11_payment(offer["payment_request"])
+    print(f"Payment made: {pay}")
+
+client = MarkdownPDF(on_payment_request=pay)
+path = client.convert("# Save this one using Alby", download_path="output.pdf") # Replace with your own unique markdown content to ensure you trigger L402.
+print("Saved PDF to:", path)
