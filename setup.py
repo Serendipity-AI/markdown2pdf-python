8e20c0ca8bf3c54253b4088ee642b70c3e67427b
Jim Marshall <jim@serendipityai.co.uk>
2025-06-11 10:52:31 +0100

    Fix packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="markdown2pdf-python",
-    version="0.1.4",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=["requests"],
     author="Serendipity AI",
