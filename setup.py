b8465c5ed398f3fb6d2823c81d60fdfda4f73e4c
Jim Marshall <jim@serendipityai.co.uk>
2025-06-13 15:53:13 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="markdown2pdf-python",
-    version="0.1.7",
+    version="0.1.10",
     packages=find_packages(),
     install_requires=["requests"],
     author="Serendipity AI",
